98ef472aabdab4c61c394deb95f705c114f16d43
agcdtmr <anjdeitmer@gmail.com>
2024-06-17 10:35:53 +0200

    create a branch for jinja2 trials

---
+++ 08-jinja2-django-docker/dockerized-jinja2-django-app/stable/core/jinja2_env.py
@@ -0,0 +1,52 @@
+from jinja2 import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
+from jinja2 import Environment, contextfunction
+
+@contextfunction
+def light_or_dark_mode(context, element):
+    request = context['request']
+    mode = request.GET.get('mode')
+    if mode == 'dark':
+        return f'''
+        <a href="{request.path}">Switch to Light Mode</a>
+        <style>
+          {element} {{
+            background-color: #212F3C;
+            color: #FFFFF0;
+          }}
+          {element} a {{
+            color: #00BFFF !important;
+          }}
+        </style>
+        '''
+    else:
+        return f'<a href="{request.path}?mode=dark">Switch to Dark Mode</a>'
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update({
+        'light_or_dark_mode': light_or_dark_mode,
+    })
+    return env
+
+
+## Default Config
+
+
+# from jinja2 import Environment
+
+# Option #1
+# def environment(**options):
+#     env = Environment(**options)
+#     # Remove or update this line
+#     # env.add_extension('jinja2.ext.with_')
+#     return env
+
+# Option #2
+# def environment(**options):
+#     env = Environment(**options)
+#     env.globals.update({
+#         'static': staticfiles_storage.url,
+#         'url': reverse,
+#     })
+#     return env
